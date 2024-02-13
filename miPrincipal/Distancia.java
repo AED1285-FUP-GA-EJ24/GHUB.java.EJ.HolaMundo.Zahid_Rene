d278b37fac3bd1afaa352bfcc8ba02cc03435ee4
Zahid Rene Jimenez Olivas <159456299+REHID-35@users.noreply.github.com>
2024-02-13 14:55:59 +0000

    Actualización_clase_distancia

---
+++ miPrincipal/Distancia.java
@@ -0,0 +1,20 @@
+package miPrincipal;
+import java.util.Scanner;
+
+public class Distancia {
+
+    public static void main(String[] args) {
+
+        Scanner leer = new Scanner(System.in);
+        float V;//Val.Entrada como real
+        float T;//Val.Entrada como real
+        float D;//Val.Salida como real
+        System.out.println("Velocidad en M/S:");
+        V = leer.nextfloat();
+        System.out.println("Segundos:");
+        T = leer.nextfloat();     
+        D=V*T;
+        System.out.println("Distancia: "+D);
+
+    }
+}
